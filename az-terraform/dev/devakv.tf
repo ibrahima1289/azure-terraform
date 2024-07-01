55f75de20cd4559cb09d4d188807d2a7a3d3a1e7
ibrahima <ibrahima.diallo1289@gmail.com>
2024-07-01 00:04:00 -0400

    adding akv rotation policy

--- az-terraform/dev/devakv.tf
+++ az-terraform/dev/devakv.tf
@@ -13,13 +13,11 @@ resource "random_string" "akv-name" {
 }
 
 resource "azurerm_key_vault" "dev-akv" {
-  name                = "${lower(var.infra)}-akv-vault"
-  resource_group_name = azurerm_resource_group.dev-state-rg.name
-  tenant_id           = data.azuread_client_config.current.tenant_id
-  location            = var.location
-  sku_name            = var.sku_name
-
-  # purge_protection_enabled = false
+  name                       = "${lower(var.infra)}-akv-vault-3"
+  resource_group_name        = azurerm_resource_group.dev-state-rg.name
+  tenant_id                  = data.azuread_client_config.current.tenant_id
+  location                   = var.location
+  sku_name                   = var.sku_name
   enable_rbac_authorization  = true
   purge_protection_enabled   = true
   soft_delete_retention_days = 7
@@ -49,17 +47,28 @@ resource "azurerm_key_vault_access_policy" "storage_mi_kv_access" {
 #   storage_account_id = azurerm_storage_account.dev-state-sa.id
 #   key_vault_id       = azurerm_key_vault.dev-akv.id
 #   key_name           = azurerm_key_vault_key.dev-key.name
+
+#   depends_on = [azurerm_key_vault_access_policy.storage]
 # }
 
 resource "azurerm_key_vault_key" "dev-key" {
-  name = "${lower(var.infra)}-key-${random_string.akv-name.result}"
-  # name            = "${lower(var.infra)}-key-${formatdate("YYYYMMDD-hhmm", timestamp())}"
-  key_vault_id = azurerm_key_vault.dev-akv.id
-  key_type     = var.key_type
-  key_size     = var.key_size
-  key_opts     = var.key_ops
-  # expiration_date = time_offset.offset
+  name            = "${lower(var.infra)}-key-${formatdate("YYYYMMDD-hhmm", timestamp())}"
+  key_vault_id    = azurerm_key_vault.dev-akv.id
+  key_type        = var.key_type
+  key_size        = var.key_size
+  key_opts        = var.key_ops
+
   expiration_date = time_offset.offset.rfc3339
+
+  # Rotate keys automatically after x months
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P18M"
+    }
+
+    expire_after         = "P2Y"
+    notify_before_expiry = "P1M"
+  }
 
   depends_on = [
     azurerm_key_vault_access_policy.storage,
@@ -67,19 +76,21 @@ resource "azurerm_key_vault_key" "dev-key" {
   ]
 
   lifecycle {
-    # create_before_destroy = false
     create_before_destroy = true
   }
 }
 
+# This will trigger a new key replacement when the Key name changes 
+# due to new date/timestamp appended to the the key name
 resource "null_resource" "cmk_replacement_trigger" {
   triggers = {
     today = formatdate("YYYYMMDD", timestamp())
   }
 }
 
+# Number of day before the key expires
 resource "time_offset" "offset" {
-  offset_days = 100
+  offset_days = 730
 
   lifecycle {
     replace_triggered_by = [null_resource.cmk_replacement_trigger]
